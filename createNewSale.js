2452bb25d979e50e8e97b46ac6864a755f8a7802
Luiz Hamilton Ribeiro Leite Soares <luizhamilton29@gmail.com>
2018-03-21 16:32:44 -0300

    Added files to create new Sale from a template. Template uses parameters from Civil Sale

---
+++ createNewSale.js
@@ -0,0 +1,35 @@
+var ejs  = require('ejs');
+var fs   = require('fs');
+var read = fs.readFileSync;
+var join = require('path').join;
+
+
+var jsonData = read(join(__dirname, '/newSaleParameters.json'), 'utf8');
+var newSaleParameters = JSON.parse(jsonData);
+
+var saleTemplate  = read(join(__dirname, '/templates/SaleTemplate.tmp'), 'utf8');
+
+var saleSourceCode = ejs.compile(saleTemplate)({
+  SaleName: newSaleParameters.SaleName,
+  TOTAL_SALE_CAP: newSaleParameters.TOTAL_SALE_CAP,
+  MIN_CONTRIBUTION: newSaleParameters.MIN_CONTRIBUTION,
+  MIN_THRESHOLD: newSaleParameters.MIN_THRESHOLD,
+  MAX_TOKENS: newSaleParameters.MAX_TOKENS,
+  CLOSING_DURATION: newSaleParameters.CLOSING_DURATION,
+  VAULT_INITIAL_AMOUNT: newSaleParameters.VAULT_INITIAL_AMOUNT,
+  VAULT_DISBURSEMENT_AMOUNT: newSaleParameters.VAULT_DISBURSEMENT_AMOUNT,
+  START_TIME: newSaleParameters.START_TIME,
+  WALLET: newSaleParameters.WALLET
+});
+
+fs.writeFileSync('./contracts/'+newSaleParameters.SaleName+'Sale.sol', saleSourceCode);
+
+var tokenTemplate  = read(join(__dirname, '/templates/TokenTemplate.tmp'), 'utf8');
+
+var tokenSourceCode = ejs.compile(tokenTemplate)({
+  SaleName: newSaleParameters.SaleName,
+  TokenSymbol: newSaleParameters.TokenSymbol,
+  TokenDecimals: newSaleParameters.TokenDecimals,
+});
+
+fs.writeFileSync('./contracts/'+newSaleParameters.SaleName+'Token.sol', tokenSourceCode);
