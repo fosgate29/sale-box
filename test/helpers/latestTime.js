4d37bdc8bfab11e8ea62bb61418f7643b539084b
Luiz Hamilton Ribeiro Leite Soares <luizhamilton29@gmail.com>
2018-03-21 20:01:00 -0300

    Removed unnecessary folders and files

--- test/helpers/latestTime.js
+++
@@ -1,9 +0,0 @@
-// Returns the time of the last mined block in seconds
-export default function latestTime() {
-  return new Promise((resolve, reject) => {
-    web3.eth.getBlock('latest', (error, result) => {
-      if (error) reject(error);
-      resolve(result.timestamp);
-    });
-  });
-}
